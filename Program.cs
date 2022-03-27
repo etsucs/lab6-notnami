41532e51202b2f849702ba58e91fbcff4088fe12
notnami <92872957+notnami@users.noreply.github.com>
2022-03-27 16:14:54 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿IsPrime();
+
+    void IsPrime()
+    {
+System.Console.WriteLine("Please enter a Number.");
+int Number = Int32.Parse( Console.ReadLine());
+  int n = Number, a = 0;
+         for (int i = 1; i <= n; i++) 
+         {
+            if (n % i == 0) {
+               a++;
+            }
+         }
+         if (a == 2) {
+            Console.WriteLine($"{Number} is a Prime Number", n);
+         } else 
+         {
+            Console.WriteLine($"{Number} is not a Prime Number");
+         }
+
+    }
+
+
+
